e760f130bc8770ffabaedf6d565d802d892b4e78
tzuhsun <hsungrass@gmail.com>
2023-10-22 10:03:06 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+import Chatbot from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AboutPage from "./about";
+import HomePage from "./home"
+import config from "./chatbot/config";
+import MessageParser from "./chatbot/MessageParser";
+import ActionProvider from "./chatbot/ActionProvider";
+import './chatbot.css'; // 导入自定义的CSS文件
+
 
 function App() {
+  const [isChatbotOpen, setChatbotOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setChatbotOpen(!isChatbotOpen);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+             <Route element={<HomePage />} path={'/'}></Route>
+             <Route element={<AboutPage />} path='/about'></Route>
+        </Routes>
+      
+      <div className="chatbot-container">
+        <div className="chatbot-button" onClick={toggleChatbot}>
+          {isChatbotOpen ? "Hide Chat" : "Show Chat"}
+        </div>
+        {isChatbotOpen && (
+          <Chatbot
+            config={config}
+            messageParser={MessageParser}
+            actionProvider={ActionProvider}
+          />
+        )}
+      </div>
+      
     </div>
   );
 }
---
+++ src/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* 在這裡添加 Chatbot 代碼 */}
+    </div>
+  );
+};
+
+export default AboutPage;
